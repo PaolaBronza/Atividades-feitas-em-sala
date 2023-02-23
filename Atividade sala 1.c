af6e3140bf4eb501a702a772c16a69919a22d368
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-02-23 09:43:19 -0300

    Rename Atividade sala 1.cpp to Atividade sala 1.c

--- Atividade sala 1.c
+++ Atividade sala 1.c
@@ -6,20 +6,20 @@
 int main (void){
 	setlocale(LC_ALL,"portuguese");
 	int num,num2;
-	printf("Digite um número de 1 a 10: ");
+	printf("Digite um nÃºmero de 1 a 10: ");
 	scanf("%d",&num);
-	printf("Digite um número de 1 a 10: ");
+	printf("Digite um nÃºmero de 1 a 10: ");
 	scanf("%d",&num2);
-	printf("Números recebidos: %d e %d",num,num2);
+	printf("NÃºmeros recebidos: %d e %d",num,num2);
 	
 	if(num==5||num2==10){
 		printf("\n5 ou 10");
 	}	
 	if(num<num2){
-		printf("\nPrimeiro é menor");
+		printf("\nPrimeiro Ã© menor");
 		}
 	if(num>num2){
-		printf("\nPrimeiro é maior");
+		printf("\nPrimeiro Ã© maior");
 	}
 	return 0;
 }
